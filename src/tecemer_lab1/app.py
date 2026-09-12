ec9512456b8a4518057ce8e6b5566f3a0245a4f3
Angelo-Paucar-UNCP <angeloelvio@gmail.com>
2026-09-12 16:44:26 -0500

    fix: elimina main.py sobrante y actualiza app.py

--- src/tecemer_lab1/app.py
+++ src/tecemer_lab1/app.py
@@ -1,6 +1,10 @@
 import requests
 
-respuesta = requests.get("https://official-joke-api.appspot.com/random_joke")
-datos = respuesta.json()
-print(datos["setup"])
-print(datos["punchline"])
+try:
+    respuesta = requests.get("https://official-joke-api.appspot.com/random_joke")
+    respuesta.raise_for_status() # Verifica si hubo un error HTTP
+    datos = respuesta.json()
+    print(datos["setup"])
+    print(datos["punchline"])
+except requests.exceptions.RequestException as e:
+    print(f"Error al conectar con la API: {e}")
